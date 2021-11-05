29c0e927709d02018b968d146b60a5c550f2a787
Vasyl <kushnir.vasyl.lv@gmail.com>
2021-11-05 12:10:44 +0200

    set zero for limit and skip, move invalid-false check to patch method

--- src/services/notifications/notifications.validations.ts
+++ src/services/notifications/notifications.validations.ts
@@ -1,11 +1,11 @@
 import Joi from 'joi';
 import { NotificationType } from '../../models/notifications.model';
 const description = Joi.string().trim().min(1).max(100);
-const isRead = Joi.boolean().invalid(false);
+const isRead = Joi.boolean();
 const type = Joi.string().valid(...Object.values(NotificationType));
 const userId = Joi.string().uuid();
-const limit = Joi.number().min(1).max(100).default(20);
-const skip = Joi.number().min(1).max(100).default(0);
+const limit = Joi.number().min(0).max(100).default(20);
+const skip = Joi.number().min(0).max(100).default(0);
 
 export const createNotificationSchema = Joi.object({
   description: description.required(),
@@ -26,5 +26,5 @@ export const getNotificationFilters = Joi.object({
 });
 
 export const updateNotificationSchema = Joi.object({
-  isRead: isRead.required(),
+  isRead: isRead.invalid(false).required(),
 });
