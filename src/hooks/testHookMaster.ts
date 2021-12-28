3930f31e9d14d3eaf2105608016c92c19839f26f
vasylkushnir <50378914+vasylkushnir@users.noreply.github.com>
2021-12-28 09:39:55 +0200

    add master hook

---
+++ src/hooks/testHookMaster.ts
@@ -0,0 +1,20 @@
+import { Hook, HookContext } from '@feathersjs/feathers';
+import { BadRequest } from '@feathersjs/errors';
+import Joi from 'joi';
+
+/**
+ * Check if id is valid UUID
+ * schemaValidate - Joi object with validation rules
+ * context.id - UUID to validate
+ */
+export default (schemaValidate: Joi.Schema): Hook => {
+  return (context: HookContext): HookContext => {
+    if (context.id) {
+      const validationObj = schemaValidate.validate({ id: context.id });
+      if (validationObj.error) {
+        throw new BadRequest(`${validationObj.error.details[0].message}`);
+      }
+    }
+    return context;
+  };
+};
